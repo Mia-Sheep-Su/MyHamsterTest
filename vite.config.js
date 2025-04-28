1608b657e7e76812a9f9daae57a364542e4c5df0
Mia Su <miasu713@gmail.com>
2025-04-29 06:57:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ⚠️ 這裡的名稱必須與你的 GitHub Repo 名稱相符！
+  base: "/MyHamsterTest/", // ⚠️ 這裡的名稱必須與你的 GitHub Repo 名稱相符！
 });
