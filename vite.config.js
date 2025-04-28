611eecf3f2650cca9810cbc585b792547e087698
Mia Su <miasu713@gmail.com>
2025-04-29 06:51:22 +0800

    更改部屬設定
    上傳 dist 到 GitHub Pages（手動做法）

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/MyHamsterTest/", // ⚠️ 這裡的名稱必須與你的 GitHub Repo 名稱相符！
+});
